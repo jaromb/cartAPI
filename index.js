b4312960bb89a0298ac0b9c6336080fe8b1d141f
Jarom Bridges <jarombridges@hotmail.com>
2019-03-28 22:39:07 -0600

    created functionality for catalog management, with edits to post to allow modification of inventory items in addition to adding, deleting

--- index.js
+++ index.js
@@ -8,51 +8,51 @@ app.use(bodyParser.json())
 
 let items = [
     {name: 'Cereal',
-    id: '1',
+    id: 'rtql91bvnkr',
     image: 'https://images-na.ssl-images-amazon.com/images/G/01/aplusautomation/vendorimages/c3d80660-ba9e-4f42-b989-a423ba979fc2.jpg._CB289958391_.jpg',
     price: '4.00'},
     {name: 'Bananas',
-    id: '2',
+    id: 'q5v4hl7at3a',
     image: 'https://img.purch.com/rc/300x200/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA2NS8xNDkvb3JpZ2luYWwvYmFuYW5hcy5qcGc=',
     price: '2.00'},
     {name:'Steak',
-    id: '3',
+    id: 'jzngrdlngf',
     image: 'https://cdn.shopify.com/s/files/1/1579/5769/products/ribeye_prime_1_1024x1024.jpg?v=1479226988',
     price: '8.00'},
     {name:'Orange Juice',
-    id: '4',
+    id: '6plkngyk23i',
     image: 'https://instamartkauai.com/wp-content/uploads/2018/04/large_add3b7ed-a890-485b-827a-294c0a3a740e-600x600.jpg',
     price: '3.50'},
     {name: 'Bread',
-    id: '5',
+    id: 'p5n3m5xud5m',
     image: 'https://e22d0640933e3c7f8c86-34aee0c49088be50e3ac6555f6c963fb.ssl.cf2.rackcdn.com/0072250037060_CL_default_default_large.jpeg',
     price: '1.00'},
     {name: 'Milk',
-    id: '6',
+    id: 'wsjv48lpgsj',
     image: 'https://static.meijer.com/Media/000/41250/0004125010210_1_A1C1_0600.png',
     price: '2.50'},
     {name: 'Tomatoes',
-    id: '7',
+    id: 'c84p825ldir',
     image: 'https://cdn.shopify.com/s/files/1/2918/4630/products/Image_3_104b31f8-80a1-44f7-b624-56a429a073f9.jpg?v=1527364299',
     price: '2.70'},
     {name: 'Cheese',
-    id: '8',
+    id: 'dub7jzkbss4',
     image: 'https://static1.squarespace.com/static/5a1592ff0abd04e470d48744/t/5a27bbeeec212d905273a14c/1512996237223/Kaas.jpeg?format=1500w',
     price: '4.00'},
     {name: 'Ice Cream',
-    id: '9',
+    id: '8x3gx0h0tt',
     image: 'https://i5.walmartimages.com/asr/05613edd-98e9-453d-9e94-cb1ebdd5a841_1.f7e72f0841619ffcb1e30e5482a23e00.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF',
     price: '2.99'},
     {name: 'Chicken',
-    id: '10',
+    id: '5cohj0on21l',
     image: 'http://robbieflexibles.com/getmedia/bb525202-dcf5-40ae-b369-03b36ed92dc5/rotisserie-website.aspx',
     price: '3.50'},
     {name: 'Pizza',
-    id: '11',
+    id: 'jxoyf0l8qqe',
     image: 'https://www.dollargeneral.com/media/catalog/product/cache/image/700x700/e9c3970ab036de70892d86c6d221abfe/0/0/00071921825881_a1l1_900_900.jpg',
     price: '5.00'},
     {name: 'Lettuce',
-    id: '12',
+    id: '3dt6a7oo73h',
     image: 'https://images-na.ssl-images-amazon.com/images/I/41CGtIyWgML._SX355_.jpg',
     price: '1.29'}]
 
@@ -70,10 +70,35 @@ app.post('/items', (req, res) => {
     console.log('/items POST end point activated')
     console.log(req.body)
     
-    req.body.id = Math.random().toString(36).substring(2, 15)
-    items.push(req.body)
+
+    if(req.body.id === undefined) {
+        console.log('ID undefined, add item')
+        req.body.id = Math.random().toString(36).substring(2, 15);
+        items.push(req.body)
+    }
+    else {
+        console.log('identify match')
+        const matchingIndex = items.findIndex(item =>
+            item.id === req.body.id
+        )
+        console.log(matchingIndex)
+        items.splice(matchingIndex, 1, req.body)
+    } 
+    // ()= (item) => {
+//     for (i=0; i<items.length; i++) {
+//     if (this.items.item.id == req.body.id) {
+//         this.items.item.name = req.body.name;
+//         this.items.item.price = req.body.price;
+//         this.items.item.image = req.body.image;
+//     }
+    
+//     }
+    
+// }
     res.send(items)
 })
+
+/////////////////////////////////////
 
 app.get('/cart', (req, res) => {
     res.send(cart)
@@ -101,17 +126,16 @@ app.post('/cart', (req, res) => {
 
 })
 
-app.patch('/items/:name', (req, res) => {
-    if(req.body.name !== '') {
-    cart.item.name = req.body.name;
-    }
-    if(req.body.price !== '') {
-    cart.item.price = req.body.price;
-    }
-    if(req.body.url !== '') {
-    cart.item.url = req.body.url
-    }
-})
+// app.patch('/items/:id', (req, res) => {
+//     cart = cart.ap(item => item.id === req.params.id ? {
+//     ...item,
+//     ...req.body
+// }) : item
+//     }
+// res.send(cart)
+// })
+
+
 
 
 
