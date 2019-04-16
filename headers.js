d1aefb31c9d3d64a3e95cc3d76ea9ff1feb4006e
Jarom Bridges <jarombridges@hotmail.com>
2019-04-15 23:14:40 -0600

    added allow-credentials

--- headers.js
+++ headers.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
-    res.header('access-control-allow-origin', '*') //who is allowed to access
-    res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE, PATCH')  //what methods are allowed
-    res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    res.header('Access-Control-Allow-Origin', '*') //who is allowed to access
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')  //what methods are allowed
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    res.header('Access-Control-Allow-Credentials', true)
     next()
   }
