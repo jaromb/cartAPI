c8518b5de0495d02fb04a064fc35da2296a9f629
Jarom Bridges <jarombridges@hotmail.com>
2019-04-16 23:24:39 -0600

    changed cors to req.headers.origin

--- headers.js
+++ headers.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'localhost:3000') //who is allowed to access
+    res.header('Access-Control-Allow-Origin', req.headers.origin) //who is allowed to access
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')  //what methods are allowed
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     res.header('Access-Control-Allow-Credentials', true)
