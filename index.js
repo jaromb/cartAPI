e7919294c91c3d8e103f50a9a60d95dc9fe2f6cf
Jarom Bridges <jarombridges@hotmail.com>
2019-04-16 22:09:51 -0600

    add res.send to login

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ app.post('/admin/login', (req, res) => {
             const token = jwt.sign(result, secret);
             console.log(token)
             res.cookie('token', token)
-            res.send(cookie)
+            res.send(result)
         })  
 })
 
