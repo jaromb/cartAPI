ce4f33253cdf603b511b83ca5e63caa429a3a2c4
Jarom Bridges <jarombridges@hotmail.com>
2019-04-21 23:50:55 -0600

    corrected cors url

--- headers.js
+++ headers.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000') //who is allowed to access
+    res.header('Access-Control-Allow-Origin', 'https://my-helio-cart.herokuapp.com') //who is allowed to access
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')  //what methods are allowed
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     res.header('Access-Control-Allow-Credentials', true)
